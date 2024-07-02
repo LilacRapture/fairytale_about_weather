8e2b960bae7cb45247a1bb93125da141bf8c1ff6
LilacRapture <lilacrapture@gmail.com>
2024-07-02 15:02:35 +0300

    Add async

--- weather.py
+++ weather.py
@@ -1,33 +1,46 @@
-import requests
+import aiohttp
+import asyncio
+from config import weather_api_key
 
 
-def get_weather_forecast(city_name: str, api_key: str):  # Функция парсит погоду по названию города
+async def get_weather_forecast(city_name: str, api_key: str) -> dict | None:  # Функция парсит погоду по названию города
     # url геолокации
     geocode_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={api_key}"
-    geocode_response = requests.get(geocode_url)
-    if geocode_response.status_code == 200:  # Если запрос прошёл успешно
-        data = geocode_response.json()
-        # Получаем высоту и широту локации
-        lat = data[0]["lat"]
-        lon = data[0]["lon"]
 
-        # url прогноза погоды по геолокации
-        weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
-        weather_response = requests.get(weather_url)  # Делаем запрос
+    async with aiohttp.ClientSession() as session:
+        async with session.get(geocode_url) as geocode_response:
+            if geocode_response.status == 200:  # Если запрос прошёл успешно
+                data = await geocode_response.json()
+                # Получаем высоту и широту локации
+                lat = data[0]["lat"]
+                lon = data[0]["lon"]
 
-        if weather_response.status_code == 200:  # Если запрос прошёл успешно
-            data = weather_response.json()
-            main = data['main']
-            weather = data['weather'][0]
-            forecast = {
-                'city': data['name'],
-                'temperature': main['temp'],
-                'pressure': main['pressure'],
-                'humidity': main['humidity'],
-                'description': weather['description']
-            }  # Формируем готовые данные о погоде
-            return forecast
-        else:
-            return None
-    else:
-        return None
+                # url прогноза погоды по геолокации
+                weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+                async with session.get(weather_url) as weather_response:  # Делаем запрос
+                    if geocode_response.status == 200:  # Если запрос прошёл успешно
+                        data = await weather_response.json()
+                        main = data['main']
+                        weather = data['weather'][0]
+                        forecast = {
+                            'city': data['name'],
+                            'temperature': main['temp'],
+                            'pressure': main['pressure'],
+                            'humidity': main['humidity'],
+                            'description': weather['description']
+                        }  # Формируем готовые данные о погоде
+                        return forecast
+                    else:
+                        return None
+            else:
+                return None
+
+
+async def main():
+    city_name = "London"
+    api_key = weather_api_key
+    f = await get_weather_forecast(city_name, api_key)
+    print(f"{f}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
